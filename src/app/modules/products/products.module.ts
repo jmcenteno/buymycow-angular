c3e8682dd75ad0163f0f42d215463848cbad15f6
Jose Centeno <jmcenteno@gmail.com>
2017-10-19 13:41:44 -0400

    import shared module

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
@@ -8,7 +9,8 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 @NgModule({
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    SharedModule
   ],
   declarations: [ProductListComponent, ProductDetailsComponent],
   exports: [
