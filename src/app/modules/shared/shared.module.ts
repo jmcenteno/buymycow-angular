18c9ae70d56bc89a5bc04e878f2a53e7be429fac
Jose Centeno <jmcenteno@gmail.com>
2017-10-19 13:43:00 -0400

    page header component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -5,18 +5,20 @@ import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule
   ],
-  declarations: [NavbarComponent, FooterComponent, PageNotFoundComponent],
+  declarations: [NavbarComponent, FooterComponent, PageNotFoundComponent, PageHeaderComponent],
   exports: [
     RouterModule,
     NavbarComponent,
     FooterComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    PageHeaderComponent
   ]
 })
 export class SharedModule {
