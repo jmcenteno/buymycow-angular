345c970646f5423287ac76c36cf575f4199e2baa
Jose Centeno <jmcenteno@gmail.com>
2017-10-19 16:10:06 -0400

    firebase config

---
+++ src/app/config/firebase.ts
@@ -0,0 +1,9 @@
+// add your own configuration here
+export const FIREBASE_CONFIG: any = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
+};
