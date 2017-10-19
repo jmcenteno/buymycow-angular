c5d2057dd3a41b56dfc63d1f0995fba0c6e608e8
Jose Centeno <jmcenteno@gmail.com>
2017-10-19 13:23:21 -0400

    import components and routing module

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ProductsRoutingModule } from './products-routing.module';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ProductsRoutingModule
   ],
-  declarations: []
+  declarations: [ProductListComponent, ProductDetailsComponent],
+  exports: [
+    ProductListComponent,
+    ProductDetailsComponent
+  ]
 })
 export class ProductsModule { }
