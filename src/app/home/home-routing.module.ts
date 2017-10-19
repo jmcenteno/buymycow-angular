10e6bbf0e4c12827b0cf20942c23150af0a5b345
Jose Centeno <jmcenteno@gmail.com>
2017-10-19 11:21:43 -0400

    home module

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,10 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+];
+
+export const HomeRoutingModule: ModuleWithProviders = RouterModule.forChild(routes);
