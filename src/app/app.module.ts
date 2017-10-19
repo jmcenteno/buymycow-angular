caf35f60f16f43d1a33e8101bb7c507647c5d09b
Jose Centeno <jmcenteno@gmail.com>
2017-10-19 11:22:23 -0400

    import other modules into main app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './home/home.module';
+import { ProductsModule } from './products/products.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    HomeModule,
+    ProductsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
