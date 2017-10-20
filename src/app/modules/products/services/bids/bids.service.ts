7f0e97b7abff29ca8d3a6799e03c64d9c2b1ccc2
Jose Centeno <jmcenteno@gmail.com>
2017-10-20 14:04:45 -0400

    create bid method

--- src/app/modules/products/services/bids/bids.service.ts
+++ src/app/modules/products/services/bids/bids.service.ts
@@ -1,15 +1,59 @@
 import { Injectable } from '@angular/core';
 import { database } from 'firebase';
 
+import { UtilitiesService } from '../../../shared/services/utilities/utilities.service';
+
 @Injectable()
 export class BidsService {
 
-  constructor() { }
+  constructor(private utils: UtilitiesService) { }
 
   getBidsByProduct(id) {
 
     return database().ref(`bids/${id}`);
     
+  }
+
+  createBid(bid: any, product: any): Promise<any[]> {
+    
+    return new Promise((resolve, reject) => {
+
+      const ref = database().ref(`/bids/${product.key}`);
+      
+      ref.once('value', (snapshot) => {
+            
+        const remainingTime = this.utils.getRemainingTime(product.endDate);
+          
+        if (remainingTime.difference && !product.sold) {
+          
+          ref.push(bid, (error) => {
+            
+            if (error) {
+            
+              reject({
+                message: 'Your bid cannot be placed at this time. Please try again later.'
+              });
+            
+            } else {
+            
+              resolve(bid);
+            
+            }
+  
+          });
+  
+        } else {
+          
+          reject({
+            message: 'This auction is no longer active' 
+          });
+  
+        }
+  
+      });
+
+    });
+
   }
 
 }
