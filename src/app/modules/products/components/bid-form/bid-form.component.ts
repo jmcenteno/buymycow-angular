3d4f9c4ae69a55674086d28439814a8f7741457e
Jose Centeno <jmcenteno@gmail.com>
2017-10-20 10:52:48 -0400

    form markup and validation

--- src/app/modules/products/components/bid-form/bid-form.component.ts
+++ src/app/modules/products/components/bid-form/bid-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { ValidationService } from '../../../shared/services/validation/validation.service';
 
 @Component({
   selector: 'app-bid-form',
@@ -14,7 +17,12 @@ export class BidFormComponent implements OnInit {
   @Output() setUser: EventEmitter<any>;
   @Output() submit: EventEmitter<any>;
 
-  constructor() {
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private validationService: ValidationService
+  ) {
 
     this.setUser = new EventEmitter<any>();
     this.submit = new EventEmitter<any>();
@@ -22,6 +30,36 @@ export class BidFormComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    this.form = this.formBuilder.group({
+      username: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      amount: new FormControl(Number.parseFloat(this.product.minimumBid).toFixed(2), Validators.compose([
+        Validators.required,
+        Validators.min(this.product.minimumBid),
+        this.validationService.isNumeric
+      ]))
+    });
+
+  }
+
+  handleSubmit(e: Event) {
+
+    e.preventDefault();
+
+    this.submit.emit(this.form.value);
+
+  }
+
+  formatAmount(value) {
+
+    const { amount } = this.form.controls;
+
+    if (amount.valid) {
+      amount.setValue(Number.parseFloat(amount.value).toFixed(2));
+    }
+
   }
 
 }
