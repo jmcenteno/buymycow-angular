044417e669b4350f9f3a0041e4e520952379ef16
Jose Centeno <jmcenteno@gmail.com>
2017-10-20 15:13:15 -0400

    finished create bid form

--- src/app/modules/products/components/bid-form/bid-form.component.ts
+++ src/app/modules/products/components/bid-form/bid-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
+import { UserService } from '../../../shared/services/user/user.service';
 import { ValidationService } from '../../../shared/services/validation/validation.service';
 
 @Component({
@@ -12,35 +13,45 @@ export class BidFormComponent implements OnInit {
 
   @Input() product: any;
   @Input() bids: any;
-  @Input() user: any;
 
-  @Output() setUser: EventEmitter<any>;
-  @Output() submit: EventEmitter<any>;
+  @Output() onSubmit: EventEmitter<any>;
 
   form: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
-    private validationService: ValidationService
+    private validationService: ValidationService,
+    private userService: UserService
   ) {
 
-    this.setUser = new EventEmitter<any>();
-    this.submit = new EventEmitter<any>();
+    this.onSubmit = new EventEmitter<any>();
 
   }
 
   ngOnInit() {
 
     this.form = this.formBuilder.group({
-      username: new FormControl('', Validators.compose([
+      user: new FormControl('', Validators.compose([
         Validators.required
       ])),
-      amount: new FormControl(Number.parseFloat(this.product.minimumBid).toFixed(2), Validators.compose([
+      amount: new FormControl(Number(this.getCurrentPrice()) + 1, Validators.compose([
         Validators.required,
-        Validators.min(this.product.minimumBid),
+        Validators.min(Number(this.getCurrentPrice()) + 1),
         this.validationService.isNumeric
       ]))
     });
+
+    this.userService.currentUser.subscribe((user) => {
+      this.form.controls['user'].setValue(user);
+    });
+
+  }
+
+  ngOnChanges() {
+
+    if (this.form) {
+      this.form.controls['amount'].setValue(Number(this.getCurrentPrice()) + 1);
+    }
 
   }
 
@@ -48,7 +59,17 @@ export class BidFormComponent implements OnInit {
 
     e.preventDefault();
 
-    this.submit.emit(this.form.value);
+    const bid: any = Object.assign({}, this.form.value, { 
+      date: (new Date()).toISOString()
+    });
+
+    this.onSubmit.emit(bid);
+
+  }
+
+  setUser(value) {
+
+    this.userService.setCurrentUser(value);
 
   }
 
@@ -57,8 +78,18 @@ export class BidFormComponent implements OnInit {
     const { amount } = this.form.controls;
 
     if (amount.valid) {
-      amount.setValue(Number.parseFloat(amount.value).toFixed(2));
+      amount.setValue(Number.parseFloat(amount.value));
     }
+
+  }
+
+  private getCurrentPrice() {
+
+    return (
+      this.bids.length ?
+      this.bids[0].amount :
+      this.product.initialPrice
+    );
 
   }
 
