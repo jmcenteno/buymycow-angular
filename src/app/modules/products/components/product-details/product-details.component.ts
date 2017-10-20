ecac1005150d1dc4b6272c1c728f0e53af4d2721
Jose Centeno <jmcenteno@gmail.com>
2017-10-19 21:45:24 -0400

    get product on init

--- src/app/modules/products/components/product-details/product-details.component.ts
+++ src/app/modules/products/components/product-details/product-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ProductsService } from '../../services/products/products.service';
+import { BidsService } from '../../services/bids/bids.service';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: any;
+  bids: any[] = [];
+  
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductsService,
+    private bidsService: BidsService
+  ) { }
 
   ngOnInit() {
+
+    this.route.params.subscribe((params: any) => {
+
+      this.productService.getProductDetails(params.id)
+        .on('value', (snapshot) => {
+
+          if (snapshot.val()) {
+
+            this.product = {
+              key: params.id,
+              ...snapshot.val()
+            };
+
+          }
+
+        });
+
+    });
+
   }
 
 }
