2b71eb893609c3545c1c5e85e2d40dc7be3ae982
Jose Centeno <jmcenteno@gmail.com>
2017-10-20 08:54:07 -0400

    get bids by product

--- src/app/modules/products/services/bids/bids.service.ts
+++ src/app/modules/products/services/bids/bids.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { database } from 'firebase';
 
 @Injectable()
 export class BidsService {
 
   constructor() { }
+
+  getBidsByProduct(id) {
+
+    return database().ref(`bids/${id}`);
+    
+  }
 
 }
