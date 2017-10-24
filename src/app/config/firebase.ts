0f6e238723150de6ea4748806f1366f4506b5a6c
Jose Centeno <jmcenteno@users.noreply.github.com>
2017-10-24 08:38:11 -0400

    Delete firebase.ts

--- src/app/config/firebase.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG: any = {
-  apiKey: "AIzaSyA_7XDmIDexdhQvrSpcnjHGU0o59HiLB_g",
-  authDomain: "buymycow.firebaseapp.com",
-  databaseURL: "https://buymycow.firebaseio.com",
-  projectId: "buymycow",
-  storageBucket: "buymycow.appspot.com",
-  messagingSenderId: "935442442430"
-};
